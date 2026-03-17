53e4d6cf4d8b93049224c7e91e6cd08e49647a2e
Senju.compsuporte <murilosilvax5@gmail.com>
2026-03-17 17:22:02 -0300

    upgrade login .1

---
+++ script.js
@@ -0,0 +1,37 @@
+function validateFields() {
+            const emailValid = isEmailvalid(); 
+            document.getElementById('login').disabled = !emailValid;
+
+            const email = document.getElementById("email").value;
+            if (!email) {
+                document.getElementById('login').disabled = true;
+            } else if (validateEmail(email)) {
+                document.getElementById('login').disabled = false;
+            } else {
+                document.getElementById('login').disabled = true;
+            }
+            //verificar se o email nao é vazio e se o email é válido
+            //se verdadeiro, entao habilitar o botao de recuperar senha
+            //se falto, entao desabilitar o botao de recuperar senha
+        }
+        
+        function validateEmail(email) {
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+        }
+
+        function isEmailvalid() {   
+
+            const email = document.getElementById("email").value;
+            if (!email) {
+                return false;
+            }
+            return validateEmail(email);
+        }
+        
+        function logar() {
+            const email = document.getElementById("email").value;
+             const senha = document.getElementById("senha").value;
+
+             console.log(email,senha)
+
+        }
